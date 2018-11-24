71f9f6e77bea0e954ed24c72a2144d9883547fa9
JWang123456 <wanggaojie913@yahoo.com>
2018-11-23 20:01:01 -0600

    M

--- vidly/index.js
+++ vidly/index.js
@@ -3,16 +3,11 @@ const winston = require("winston");
 const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-const error = require("./middleware/error");
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+
+require('./startup/routes')(app);
 
 process.on("uncaughtException", ex => {
   winston.error(ex.message, ex);
@@ -41,15 +36,7 @@ mongoose
   .then(() => console.log("Connected to mongodb"))
   .catch(err => console.error("Could not connect to mongodb", err));
 
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
 
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
